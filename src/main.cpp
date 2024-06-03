af8cd6c5f8f2c4fc3e8fba7ea0084fc1afa3b142
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2024-06-03 21:43:36 +1000

    feat: add blue LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 #include "LED.h"
 
-LED RED(13, 100, 2000);
-LED GREEN(12, 100, 1000);
+
+// A bar is 2000ms
+LED RED(13, 100, 2000); // Blink once per bar
+LED GREEN(12, 100, 1000); // Blink twice per bar
+LED BLUE(11, 100, 500); // Blink four times per bar
 
 void updateLED(LED& led);
 
@@ -14,6 +17,7 @@ void setup() {
 void loop() {
     updateLED(RED);
     updateLED(GREEN);
+    updateLED(BLUE);
 }
 
 void updateLED(LED &led) {
